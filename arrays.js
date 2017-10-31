3b50abe81a25e8f81b98c4708a04ad1c814d6203
Tim Shipman <bricklayertim@yahoo.com>
2017-10-31 18:00:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = [
+  "snickers",
+  "hundred gran",
+  "kitkat",
+  "skittles"
+];
+
+var addElementToBeginningOfArray = ["array","element"];
+
+addElementToBeginningOfArray.unshift("1");
